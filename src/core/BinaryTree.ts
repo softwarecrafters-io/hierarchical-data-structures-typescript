c908688c994c51f2ac50525bce39cb1322a4e438
Miguel Angel Gomez Alvarez <miguelghz@Miguels-MacBook-Air.local>
2021-09-11 19:08:11 +0100

    green: transforms a given array to a binary tree by iterating transversal

--- src/core/BinaryTree.ts
+++ src/core/BinaryTree.ts
@@ -1,83 +1,128 @@
 import { Maybe } from 'monet';
 
-type BinaryTree<T> = {
-	root: Maybe<T>;
-	left: Maybe<BinaryTree<T>>;
-	right: Maybe<BinaryTree<T>>;
-};
-
-export class BalancedBinaryTree<T> {
-	private readonly root: T;
-	private left: Maybe<BalancedBinaryTree<T>>;
-	private right: Maybe<BalancedBinaryTree<T>>;
+export class BinaryTree<T> {
+	root: T;
+	left: BinaryTree<T>;
+	right: BinaryTree<T>;
 
 	private constructor(element: T) {
 		this.root = element;
 	}
 
 	static create<T>(element: T) {
-		return new BalancedBinaryTree(element);
+		return new BinaryTree(element);
 	}
 
-	private setLeft(element: T) {
-		return (this.left = Maybe.Just(BalancedBinaryTree.create(element)));
+	setLeft(element: T) {
+		return (this.left = BinaryTree.create(element));
+	}
+
+	setRight(element: T) {
+		return (this.right = BinaryTree.create(element));
 	}
 
 	insert(element: T) {
-		if (this.left.isNothing()) {
+		if (this.left == null) {
 			this.setLeft(element);
 		}
 	}
 }
 
-class TreeNode<T> {
-	left: TreeNode<T> = null
-	right: TreeNode<T> = null
-	constructor(public value: T) {
-		this.left = null;
-		this.right = null;
-	}
-}
-
-export class BST<T> {
-	root: TreeNode<T> = null
-
-	addNode(value:T) {
-		this.root = this.dfs(this.root, value);
-	}
-
-	dfs(node:TreeNode<T>, value:T) {
+export class BinaryTreeQuery {
+	static calculateHeight<T>(node: BinaryTree<T>) {
 		if (node == null) {
-			return new TreeNode(value);
+			return 0;
 		}
-		if(node != null){
-			node.right = this.dfs(node.right, value);
+		const heightLeftNode = this.calculateHeight(node.left);
+		const heightRightNode = this.calculateHeight(node.right);
+		return Math.max(heightLeftNode, heightRightNode) + 1;
+	}
+
+	static calculateLevel<T>(node: BinaryTree<T>) {
+		if (node == null || node.root == null) {
+			return 0;
 		}
-		else {
-			node.left = this.dfs(node.left, value);
+		return this.calculateLevel(node.left) + 1;
+	}
+
+	static isLeaf<T>(node: BinaryTree<T>) {
+		if (node == null) {
+			return false;
 		}
-		return node;
+		return node.left == null && node.right == null;
 	}
 
-	addNodes(arr) {
-		arr.forEach(val => {
-			this.addNode(val);
-		})
+	static isNode<T>(node: BinaryTree<T>) {
+		return !(node == null || this.isLeaf(node));
 	}
 
-	toString() {
-		return this.dfsa(this.root);
+	static calculateNumberOfNodes<T>(node: BinaryTree<T>) {
+		if (!this.isNode(node)) {
+			return 0;
+		}
+		const numberOfLeftNodes = this.calculateNumberOfNodes(node.left);
+		const numberOfRightNodes = this.calculateNumberOfNodes(node.right);
+		return numberOfLeftNodes + numberOfRightNodes + 1;
 	}
 
-	dfsa(node:TreeNode<T>) {
-		if(node === null)return null
-		this.printNode(node);
-		this.dfsa(node.left);
-		this.dfsa(node.right);
-	}
-
-	printNode(node:TreeNode<T>){
-		node.value.toString()
+	static isBalanced(node: BinaryTree<string>): boolean {
+		if (node == null) {
+			return true;
+		}
+		const heightLeftNode = this.calculateHeight(node.left);
+		const heightRightNode = this.calculateHeight(node.right);
+		const differenceBetweenSides = Math.abs(heightLeftNode - heightRightNode);
+		const areBalancedLeaves = differenceBetweenSides <= 1;
+		return areBalancedLeaves && this.isBalanced(node.left) && this.isBalanced(node.right);
 	}
 }
 
+export class BinaryTreeMapper {
+	static iterateTransversal<T>(node: BinaryTree<T>, level: number): T[] {
+		if (node == null) {
+			return [null];
+		}
+		if (level == 0) {
+			return [node.root];
+		}
+		if (level > 0) {
+			return BinaryTreeMapper.iterateTransversal(node.left, level - 1).concat(
+				BinaryTreeMapper.iterateTransversal(node.right, level - 1)
+			);
+		}
+	}
+
+	static toArrayTransversal<T>(node: BinaryTree<T>): T[] {
+		const height = BinaryTreeQuery.calculateHeight(node);
+		const levels = Array.from({ length: height }, (_, i) => i);
+		const treeAsArray = levels.flatMap((level) => BinaryTreeMapper.iterateTransversal(node, level));
+		return this.cleanLastNullPosition(treeAsArray);
+	}
+
+	private static cleanLastNullPosition<T>([head, ...tail]: T[]): T[] {
+		if (head == null) {
+			return [];
+		}
+		const isLastPositionNull = tail.lastIndexOf(null) === tail.length - 1;
+		if (!isLastPositionNull) {
+			return [head, ...tail];
+		}
+		const firstNullIndex = [head, ...tail].indexOf(null);
+		return [head, ...tail].slice(0, firstNullIndex);
+	}
+
+	static fromArrayToBinaryTreeTransversal<T>(elements: T[], root = BinaryTree.create(null), index = 0): BinaryTree<T> {
+		if (index < elements.length) {
+			root = BinaryTree.create(elements[index]);
+			const indexLeft = 2 * index + 1;
+			if (indexLeft < elements.length) {
+				root.left = this.fromArrayToBinaryTreeTransversal(elements, root.left, indexLeft);
+			}
+			const indexRight = 2 * index + 2;
+			if (indexRight < elements.length) {
+				root.right = this.fromArrayToBinaryTreeTransversal(elements, root.right, indexRight);
+			}
+		}
+		return root;
+	}
+}
--- src/tests/BinaryTree.test.ts
+++ src/tests/BinaryTree.test.ts
@@ -1,9 +1,101 @@
-import { BST } from '../core/BinaryTree';
+import { BinaryTree, BinaryTreeMapper, BinaryTreeQuery } from '../core/BinaryTree';
 
-test('binary tree', () => {
-	const binaryTree = new BST()
+describe('The binary tree query', () => {
+	it('calculates the greatest distance between root and any of the leaves (height) for a given binary tree', () => {
+		const binaryTree = createPerfectBinaryTreeWithOneLevel();
+		binaryTree.left.setLeft('d');
 
-	binaryTree.addNodes([1,2,3,4,5,6,1,2,3,4,5,6])
-	console.log(binaryTree)
-	expect(true).toBe(true);
+		const height = BinaryTreeQuery.calculateHeight(binaryTree);
+		expect(height).toBe(3);
+	});
+
+	it('evaluates if a given node does not have children (leaf) for a given binary tree', () => {
+		const binaryTree = createPerfectBinaryTreeWithOneLevel();
+
+		expect(BinaryTreeQuery.isLeaf(binaryTree)).toBeFalsy();
+		expect(BinaryTreeQuery.isLeaf(binaryTree.right)).toBeTruthy();
+	});
+
+	it('calculates the number of nodes for a given binary tree', () => {
+		const binaryTree = createPerfectBinaryTreeWithOneLevel();
+		//level 2
+		binaryTree.left.setLeft('d');
+		binaryTree.right.setLeft('e');
+
+		const numberOfNodes = BinaryTreeQuery.calculateNumberOfNodes(binaryTree);
+		expect(numberOfNodes).toBe(3);
+	});
+
+	it('evaluates if a given binary tree is balanced', () => {
+		const binaryTree = createPerfectBinaryTreeWithOneLevel();
+		//level 2
+		binaryTree.left.setLeft('d');
+
+		const isBalanced = BinaryTreeQuery.isBalanced(binaryTree);
+
+		expect(isBalanced).toBeTruthy();
+	});
 });
+
+describe.only('Binary tree mapper', () => {
+	it('iterates transversal by level', () => {
+		const binaryTree = createPerfectBinaryTreeWithOneLevel();
+
+		const result = BinaryTreeMapper.iterateTransversal(binaryTree, 1);
+
+		expect(result).toEqual(['b', 'c']);
+	});
+
+	it('transforms a given perfect binary tree into an array by iterating transversal', () => {
+		const binaryTree = createPerfectBinaryTreeWithTwoLevels();
+
+		const result = BinaryTreeMapper.toArrayTransversal(binaryTree);
+		expect(result).toEqual(['a', 'b', 'c', 'd', 'e', 'f', 'g']);
+	});
+
+	it('transforms a given complete binary tree into an array by iterating transversal', () => {
+		const binaryTree = createPerfectBinaryTreeWithOneLevel();
+		//level 2
+		binaryTree.left.setLeft('d');
+		binaryTree.left.setRight('e');
+
+		const result = BinaryTreeMapper.toArrayTransversal(binaryTree);
+		expect(result).toEqual(['a', 'b', 'c', 'd', 'e']);
+	});
+
+	it('transforms a given balance binary tree into an array by iterating transversal', () => {
+		const binaryTree = createPerfectBinaryTreeWithOneLevel();
+		//level 2
+		binaryTree.left.setLeft('d');
+
+		const result = BinaryTreeMapper.toArrayTransversal(binaryTree);
+		expect(result).toEqual(['a', 'b', 'c', 'd']);
+	});
+
+	it.only('transforms a given array to a binary tree by iterating transversal', () => {
+		const elements = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
+
+		const result = BinaryTreeMapper.fromArrayToBinaryTreeTransversal(elements);
+
+		expect(result).toEqual(createPerfectBinaryTreeWithTwoLevels());
+	});
+});
+
+function createPerfectBinaryTreeWithOneLevel() {
+	const binaryTree = BinaryTree.create('a');
+	//level 1
+	binaryTree.setLeft('b');
+	binaryTree.setRight('c');
+
+	return binaryTree;
+}
+
+function createPerfectBinaryTreeWithTwoLevels() {
+	const binaryTree = createPerfectBinaryTreeWithOneLevel();
+	//level 2
+	binaryTree.left.setLeft('d');
+	binaryTree.left.setRight('e');
+	binaryTree.right.setLeft('f');
+	binaryTree.right.setRight('g');
+	return binaryTree;
+}
